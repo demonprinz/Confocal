eb475acec800420676765dae73c6fdfe7ab2468a
demonprinz <scholz.patrick1995@web.de>
2024-09-26 22:03:18 +0200

    Reworked sorting the images by channels

--- pythonProject/Confocal_main.py
+++ pythonProject/Confocal_main.py
@@ -1,6 +1,6 @@
 # Version 1.0
 # author: Patrick Scholz Contact: patrick.alexander.scholz@rwth-aachen.de
-#currently intended to work for up to 10 simultaneously measured channels
+#currently intended to work for up to 99 simultaneously measured channels
 
 # import needed modules
 import imageio as iio
@@ -8,46 +8,42 @@ import numpy as np
 import promptlib
 import os
 
+def sortNew (imagename):
+    if imagename.endswith(".tif"):
+        channelnumber = int(imagename[-6:-4])
+        path = "\\" + imagename
+        channels[channelnumber].append(path)
+
+def getNumberOfChannels(imgDirectory):
+    numberOfChannels = []
+    for file in os.listdir(imgDirectory):
+        filename = os.fsdecode(file)
+        filenameEnding = filename[-6:]
+        if filenameEnding in numberOfChannels:
+            break
+        else:
+            if filenameEnding.endswith(".tif"):
+                numberOfChannels.append(filenameEnding)
+    return len(numberOfChannels)
+
 # read image files
 # Image file name format: SeriesXXX_tYYY_z0_ch0Z with XXX as increment of the run per day, YYY as timeframe and Z as Channel number
 
-#create list into which the image get sorted by channel
-channels = [[] for i in range(10)]
 #request folder path
 prompter = promptlib.Files()
 #write path into string
 imgFolderPath = prompter.dir()
 #create directory object
 imgDirectory = os.fspath(imgFolderPath)
+#create list into which the image get sorted by channel
+channels = [[] for i in range(getNumberOfChannels(imgDirectory))]
 
-def sort (channelnumber, imagename):
-    path = "\\" + imagename
-    channels[channelnumber].append(path)
 
 for file in os.listdir(imgDirectory):
     filename = os.fsdecode(file)
+
     #sorting images into their channels
-    match filename[-5]:
-        case "0":
-            sort(0, filename)
-        case "1":
-            sort(1, filename)
-        case "2":
-            sort(2, filename)
-        case "3":
-            sort(3, filename)
-        case "4":
-            sort(4, filename)
-        case "5":
-            sort(5, filename)
-        case "6":
-            sort(6, filename)
-        case "7":
-            sort(7, filename)
-        case "8":
-            sort(8, filename)
-        case "9":
-            sort(9, filename)
+    sortNew(filename)
 
 
 #imagepaths in list structure
@@ -79,3 +75,4 @@ for i in range(len(imgList)):
         stack = np.stack(imgList[i], axis=0)
         imgStack.append(stack)
 print('Volume dimensions:', imgStack[0].shape)
+print('Volume dimensions:', imgStack[1].shape)
